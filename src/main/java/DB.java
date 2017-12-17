0ac746b15faf49343984707dfa1fce31d45f8941
mulukadan <kadan.md@gmail.com>
2017-12-17 19:54:01 +0300

    Deploying to Heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,35 @@
 import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kadan", "Kadan");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kadan", "Kadan");
+
+  private static URI dbUri;
+    public static Sql2o sql2o;
+
+    Logger logger = LoggerFactory.getLogger(DB.class);
+
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "kadan" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "Kadan" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            // logger.error("Unable to connect to database.");
+        }
+    }
 }
